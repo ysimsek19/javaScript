6a98890d77b46d0070202eacaa6fca5115f1ab9b
yalcinsimsek <ysimsek@karatekin.edu.tr>
2021-03-26 18:23:27 +0300

    image eklendi

--- main1.js
+++ main1.js
@@ -25,6 +25,25 @@ document.getElementById("pizzafiyati").innerHTML=parseInt(fiyat)*parseInt(pizza)
 }
 
 function secilen(){
-    var radios = document.querySelectorAll('input[type=radio][name="islem"]');
+    var radios = document.querySelectorAll('input[type=radio][value="25"]');
     document.getElementById("secimfiyati").innerHTML=parseInt(fiyat)*parseInt(pizza);
 }
+
+$(document).ready(function () {
+       $('input[type=radio]').click(function () {
+          // document.getElementById('price').innerHTML = $(this).val();
+          var val1=$(this).val();
+          if(val1=="25"){
+              $(".resim").attr("src","image/pizzaS.png")
+          }
+          else if(val1=="20"){
+            $(".resim").attr("src","image/lamba.gif")
+          }
+          else if(val1=="15"){
+            $(".resim").attr("src","image/ampul.gif")  
+          }
+          var pizza=document.getElementById("pizza").value;
+          document.getElementById("pizzafiyati").innerHTML=parseInt(val1)*parseInt(pizza);
+       });
+
+   });
